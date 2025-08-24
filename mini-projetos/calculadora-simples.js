a8134b88f5f5608d10f3da879e874515c1df3955
Bruna <brunasilva.sms@gmail.com>
2025-08-24 20:06:59 -0300

    first commit

--- mini-projetos/calculadora-simples.js
+++ mini-projetos/calculadora-simples.js
@@ -7,27 +7,43 @@ function calcular(n1,n2){
         soma: n1+n2,
         subtracao: n1-n2,
         multiplicacao: n1*n2,
-        divisao: n1/n2,
+        divisao: n2 !== 0 ? n1 / n2 : null, // evita divisão por zero
     }
 }
 
-let tipo = entrada ('Qual operação ddeseja realizar? (1: soma, 2: subtração, 3: multiplicação ou 4: divisão');
-let n1 = Number(entrada('Digite o primeiro valor: '));
-let n2 = Number(entrada('Digite o segundo valor: '));
-let resultado = calcular(n1,n2)
+let tipo;
 
 do{
-    if (tipo = soma){
-        console.log(`O reultado da soma é: ${resultado.soma}`),
-    }else if (tipo = subtracao){
-        console.log(`O reultado da subtração é: ${resultado.subtracao}`),
-    }else if (tipo = multiplicacao){
-        console.log(`O reultado da multiplicação é: ${resultado.multiplicacao}`),
-    }else if (tipo = divisao){
-        if(num=0){
-            console.log('O valor não pode ser dividido')
-        }
-    } else{
-        console.log(`O reultado da divisão é: ${resultado.divisao}`)
+    tipo = entrada ('Qual operação deseja realizar (soma: + ,subtração: - ,multiplicação: *  ou divisão: / ? ). Para sair, digite "Sair" ').toLowerCase();
+
+    if(tipo === 'sair'){
+        console.log('Programa encerrado.');
+        break
     }
-}while
+
+    if (!['+', '-', '*', '%']) {
+        console.log('Operação inválida. Tente novamente.');
+        continue
+    }
+
+    let n1 = Number(entrada('Digite o primeiro valor: '));
+    let n2 = Number(entrada('Digite o segundo valor: '));
+    let resultado = calcular(n1,n2)
+
+    if (tipo === '+'){
+        console.log(`O reultado da soma é: ${resultado.soma}`);
+    } else if (tipo === '-'){
+        console.log(`O reultado da subtração é: ${resultado.subtracao}`);
+    } else if (tipo = '*'){
+        console.log(`O reultado da multiplicação é: ${resultado.multiplicacao}`);
+    } else if (tipo === '/'){
+        if(resultado.divisao === null){
+            console.log('Divisão por 0 não é permitida');
+        }else{
+            console.log(`O resultado da divisão é: ${resultado.divisao}`);
+        }
+    }
+}while(true);
+
+// break – Interrompe o laço completamente (sai do loop)
+// continue – Pula para a próxima iteração (volta para o inicio do loop)
