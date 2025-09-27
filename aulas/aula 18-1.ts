497ba939836d993b826f208a72df5c6ce436c4d0
Bruna Silva <brunasilva.sms@gmail.com>
2025-09-27 15:22:00 -0300

    Create aula 18-1.ts

---
+++ aulas/aula 18-1.ts
@@ -0,0 +1,22 @@
+class CadatroUsuario{
+    registar(nome:string, senha: string): void{
+        if(senha.length <6){
+            throw new Error ('Senha curta. A senha deve ter mais de 6 caracteres');
+        }
+        console.log('usuario ${nome} registrado com sucesso!');
+    }
+}
+
+try{
+    const cadastro = new CadatroUsuario();
+    cadastro.registar('João', '123');
+}catch (erro){
+    console.log('Error: ', (erro as Error).message);
+}
+
+try{
+    const cadastro = new CadatroUsuario();
+    cadastro.registar('Maria', 'abc123');
+}catch (erro){
+    console.log('Erro: ', (erro as Error).message);
+}
